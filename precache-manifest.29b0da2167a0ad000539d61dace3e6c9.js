48a9341ed7861e8ff3ec2c3b43fbe8637d851059
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-05-24 15:34:49 +0200

    Updates

---
+++ precache-manifest.29b0da2167a0ad000539d61dace3e6c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bffcb3005ca6d3e9a23cf40e2d71d8",
+    "url": "/places/index.html"
+  },
+  {
+    "revision": "f724ab17bcfe11acabdf",
+    "url": "/places/static/css/main.121f05a1.chunk.css"
+  },
+  {
+    "revision": "a94b02cc7324b66b1a41",
+    "url": "/places/static/js/2.eb8bb313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/places/static/js/2.eb8bb313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f724ab17bcfe11acabdf",
+    "url": "/places/static/js/main.dc790f1b.chunk.js"
+  },
+  {
+    "revision": "45a476ae46672333469f",
+    "url": "/places/static/js/runtime-main.d2c749bb.js"
+  },
+  {
+    "revision": "dcea39ad03938a4bf1f804706b4280df",
+    "url": "/places/static/media/foursquare.dcea39ad.svg"
+  },
+  {
+    "revision": "9367e4e38fa691cc35e043edc571e282",
+    "url": "/places/static/media/google.9367e4e3.svg"
+  },
+  {
+    "revision": "b6b9199120bf9e9506ba91b140a0e681",
+    "url": "/places/static/media/menu.b6b91991.svg"
+  }
+]);
